ddd77c033944da31a529402f0459de80016b0081
Nicola Boyle <nicola162@gmail.com>
2023-01-20 14:22:51 +0000

    created file structure and added a JS file containing quix questions

---
+++ JS/questions.js
@@ -0,0 +1,52 @@
+// Create array containing quiz question and answers
+
+var questions = [
+    // First question
+    {
+        Question: 'How do you create a function in JavaScript?',
+        ChoiceA: 'function:myFunction()',
+        ChoiceB: 'function = myFunction()',
+        ChoiceC: 'function myFunction()',
+        CorrectAnswer: 'function myFunction()'
+
+    },
+
+    // Second question
+    {
+        Question: 'What of these demonstrates the correct syntax for an array?',
+        ChoiceA: 'var fruits = ["apple", "orange", "banana"]',
+        ChoiceB: 'var fruits "apple", "orange", "banana"',
+        ChoiceC: 'var fruits = ("apple", "orange", "banana")',
+        ChoiceD: 'var fruits = [1:"apple", 2:"orange", 3:"banana"]',
+        CorrectAnswer: 'var fruits = ["apple", "orange", "banana"]'
+    }
+
+    // Third question
+    {
+        Question: 'What is JavaScript?',
+        ChoiceA: 'JavaScript is an assembly language used to make the website interactive',
+        ChoiceB: 'JavaScript is a scripting language used to make the website interactive',
+        ChoiceC: 'JavaScript is a compiled language used to make the website interactive',
+        ChoiceD: 'All of the above',
+        CorrectAnswer: 'JavaScript is a scripting language used to make the website interactive'
+    }
+
+    // Fourth question
+    {
+        Question: 'How would you add a comment in JavaScript?',
+        ChoiceA: '//This is a JavaScript comment',
+        ChoiceB: '<!-- This is a JavaScript comment -->',
+        ChoiceC: '/*This is a JavaScript comment*/',
+        CorrectAnswer: '//This is a JavaScript comment'
+    }
+
+    // Fifth question
+    {
+        Question: 'Which of the following is not a kind of loop used in JavaScript?',
+        ChoiceA: 'For loop',
+        ChoiceB: 'While loop',
+        ChoiceC: 'Do while loop',
+        ChoiceD: 'When loop',
+        CorrectAnswer: 'When loop'
+    }
+]
