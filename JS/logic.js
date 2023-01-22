e49941d76498de0f71bf0023d921acfcfb5eab2e
Nicola Boyle <nicola162@gmail.com>
2023-01-22 20:58:13 +0000

    pseudocode for logic file

---
+++ JS/logic.js
@@ -0,0 +1,31 @@
+// Declare variables
+
+    // Event listener to begin game
+
+// Function to generate and show questions at random
+
+    // Timer to begin - 60 seconds
+
+    // Timer to begin countdown to zero
+
+    // Query select buttons, add event listener to check for correct answer upon user clicking a button
+
+    //  Variable to compare answer chosen to correct answer?
+
+    // If correct, score increases, ELSE if incorrect deduct time
+
+    //Generate next question at random
+
+// Function to end the game
+
+    // Show last screen of the game and display user's score
+
+// Function to submit score
+
+// Add score to high score list within an object, use local storage to call score list and allow user to store initials with their score
+
+    // Save the list into local storage with the addition of the user's initials and new score
+
+// Add a timer function
+
+    // Set timer to deduct ten seconds for an incorrect answer
